365b3ed181f0f6284533c5b84e46017d25c66cd5
Hydr46605 <bryamjamb@proton.me>
2026-06-10 11:45:52 +0200

    fix(metadata): align public version with 0.8.3

--- lattice-core/src/main/java/dev/beryl/lattice/Lattice.java
+++ lattice-core/src/main/java/dev/beryl/lattice/Lattice.java
@@ -4,7 +4,7 @@ import dev.beryl.lattice.lifecycle.LatticeBuilder;
 import dev.beryl.lattice.lifecycle.LatticeRuntime;
 
 public final class Lattice {
-    public static final String VERSION = "0.8.2";
+    public static final String VERSION = "0.8.3";
 
     private Lattice() {
     }
